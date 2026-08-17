82f4df23bb987c4dc8626723e23bbdf2738034f9
alukov1c <a.lukovic26@gmail.com>
2026-08-17 17:18:49 +0200

    a10

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const publicFiles = new Map([
   ['/', ['index.html', 'text/html; charset=utf-8']],
   ['/index.html', ['index.html', 'text/html; charset=utf-8']],
   ['/stil.css', ['stil.css', 'text/css; charset=utf-8']],
-  ['/app.js', ['app.js', 'text/javascript; charset=utf-8']]
+  ['/app.js', ['app.js', 'text/javascript; charset=utf-8']],
+  ['/AL-AI.svg', ['AL-AI.svg', 'image/svg+xml']]
 ]);
 
 function sendJson(response, status, body) {
